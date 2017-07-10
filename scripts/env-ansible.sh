777e2bc8a9d36e496141654fed9508dc209e23ea
dvandoor <dvandoor AT cisco Dot com>
2017-07-10 08:10:47 -0400

    disable Ansible retry files. set the ansible_managed value

--- scripts/env-ansible.sh
+++ scripts/env-ansible.sh
@@ -7,6 +7,9 @@
 # This script is based on the Apache Licensed openstak-ansible script created and (c) 2014, Kevin Carter <kevin.carter@rackspace.com>
 
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:${PATH}"
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
 
 if [ $# -lt 1 ]; then
 	echo "Usage: $0 <PROJECT>"
@@ -14,7 +17,7 @@ if [ $# -lt 1 ]; then
 	echo "You must specify the project name to load the inventory and variable files for."
 	echo "     ie. $0 sample"
 	echo
-	echo "The variable files must be named <PROJECT>.config"
+	echo "The variable files must be named <PROJECT>.config and <PROJECT>.secret"
 	echo "The inventory file must be named <PROJECT>.hosts"
 	exit 1
 fi
@@ -24,8 +27,14 @@ function info() {
 }
 
 # Discover the variable files.
-VARS="-e @envs/$1.config"
+#VARS="-e @envs/$1.config -e @envs/$1.secret"
 INV="-i envs/$1.hosts"
+CONFIGFILES=`find envs -name $1.* -type f | grep -v hosts`
+
+for FILE in $CONFIGFILES
+do
+	VARS+="-e @$FILE "
+done
 
 # Shift the command line varibles to pass along to ansible-playbook
 shift
